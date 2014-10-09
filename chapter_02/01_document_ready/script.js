f6e1b39e56bc419ca7654666eb4a4bfdb9340002
aw2802 <aw2802@barnard.edu>
2014-10-08 20:06:10 -0400

    toogle button

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
   $("#celebs tbody tr:even").addClass("zebra")
 
-
   $('#hideButton').click(function(){
   	$('#disclaimer').hide();        
+  })
+
+  $('#showButton').click(function(){
+  	$('#disclaimer').show();
   })
 });
