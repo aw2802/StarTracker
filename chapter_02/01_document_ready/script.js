c4d5acadba8782952f36c3d247cd0a37e0eda310
aw2802 <aw2802@barnard.edu>
2014-10-08 20:57:56 -0400

    adding toggle button
    adding toggle button through js instead of html

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -1,31 +1,18 @@
 $(document).ready(function(){
   $("#celebs tbody tr:even").addClass("zebra")
 
-  // $('#hideButton').click(function(){
-  // 	$('#disclaimer').hide();        
-  // })
-
-  // $('#showButton').click(function(){
-  // 	$('#disclaimer').show();
-  // })
-
-// $('#toggleButton').click(function(){
-// 	if($('#disclaimer').is(':visible')){
-// 		$('#disclaimer').hide();
-// 	}
-// 	else{
-// 		$('#disclaimer').show();
-// 	}
-//  })
-
- 	// $('#toggleButton').click(function(){
- 	// 	$('#disclaimer').toggle();
- 	// 	if($("#disclaimer").is('visible')){
- 	// 		$(this).val('hide');
- 	// 	}
- 	// 	else{
- 	// 		$(this).val('show');
- 	// 	}
- 	// })
+ 	$('#toggleButton').click(function(){
+ 		$('#disclaimer').toggle();
+ 		if($("#disclaimer").is('visible')){
+ 			$(this).val('hide');
+ 		}
+ 		else{
+ 			$(this).val('show');
+ 		}
+ 	})
+ 	$("<input type='button' id='toggleButton' value='hide or show'/>").insertAfter('#disclaimer');
+ 	$('#toggleButton').click(function(){
+ 		$('#disclaimer').toggle();
+ 	})
 
 });
