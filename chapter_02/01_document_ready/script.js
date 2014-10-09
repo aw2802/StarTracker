d2cbcb1949d749d1a21b37782a87bddf390bc77f
aw2802 <aw2802@barnard.edu>
2014-10-08 20:17:39 -0400

    toogling

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
   $("#celebs tbody tr:even").addClass("zebra")
 
-  $('#hideButton').click(function(){
-  	$('#disclaimer').hide();        
-  })
+  // $('#hideButton').click(function(){
+  // 	$('#disclaimer').hide();        
+  // })
 
-  $('#showButton').click(function(){
-  	$('#disclaimer').show();
-  })
+  // $('#showButton').click(function(){
+  // 	$('#disclaimer').show();
+  // })
+
+$('#toggleButton').click(function(){
+	if($('#disclaimer').is(':visible')){
+		$('#disclaimer').hide();
+	}
+	else{
+		$('#disclaimer').show();
+	}
+ })
 });
